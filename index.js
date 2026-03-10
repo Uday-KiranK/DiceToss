84bd670134765ae2890ca7e3b28a0832e3c1073f
Uday-KiranK <uday.kottakota.k@gmail.com>
2026-03-11 03:41:36 +0530

    Modified and Optimized the Scripts

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 var rand1 = Math.floor(Math.random()*6) + 1;
 var rand2 = Math.floor(Math.random()*6) + 1;
 var imgs = document.querySelectorAll("img");
-
-if (rand1 === 1){
-    imgs[0].setAttribute("src","./images/dice1.png");
-}else if (rand1 === 2){
-    imgs[0].setAttribute("src","./images/dice2.png");
-}else if (rand1 === 3){
-    imgs[0].setAttribute("src","./images/dice3.png");
-}else if (rand1 === 4){
-    imgs[0].setAttribute("src","./images/dice4.png");
-}else if (rand1 === 5){
-    imgs[0].setAttribute("src","./images/dice5.png");
-}else{
-    imgs[0].setAttribute("src","./images/dice6.png");
-}
-
-if (rand2 === 1){
-    imgs[1].setAttribute("src","./images/dice1.png");
-}else if (rand2 === 2){
-    imgs[1].setAttribute("src","./images/dice2.png");
-}else if (rand2 === 3){
-    imgs[1].setAttribute("src","./images/dice3.png");
-}else if (rand2 === 4){
-    imgs[1].setAttribute("src","./images/dice4.png");
-}else if (rand2 === 5){
-    imgs[1].setAttribute("src","./images/dice5.png");
-}else{
-    imgs[1].setAttribute("src","./images/dice6.png");
-}
+var img0 = "./images/dice" + rand1 + ".png";
+var img1 = "./images/dice" + rand2 + ".png";
+imgs[0].setAttribute("src",img0);
+imgs[1].setAttribute("src",img1);
 
 if (rand1 === rand2){
     document.querySelector("h1").textContent = "Draw!";
